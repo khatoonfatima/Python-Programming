c836ac61a1fc55b576b729bc2a0ff246bc591b37
Fatima Khatoon <41826858+khatoonfatima@users.noreply.github.com>
2019-05-10 21:26:02 +0530

    IF  and ELSE statements

---
+++ Decision Making.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#Decision Making
+#if statement:An if statement consists of a boolean expression followed by one or more statements
+#if else:An if statement can be followed by an optional else statement, which executes when the boolean expression is FALSE.
+#nested if :You can use one if or else if statement inside another if or else if statement(s).
+var=100
+if(var==10):
+    print("the value of expression is 100")
+print("GoodBye!")
+
+
+# In[2]:
+
+
+i=10
+if(i== 5):
+    print("i is equal to 5")
+print("Hence proved")
+    
+
+
+# In[6]:
+
+
+x=3
+if(x>10):
+    print("x is smaller than 10")
+else:
+    print("x is greater than 10 or equal ")
+
+
+# In[5]:
+
+
+age=23
+print("Guess my age")
+
+guess=int(input("Guess.."))
+
+if guess!=age:
+    print("Wrong!")
+else:
+    print("you are young")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
