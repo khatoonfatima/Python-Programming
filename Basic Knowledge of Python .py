fb41659c4d6192a9e6e6b7d1d7b353bd77b2a0fe
Fatima Khatoon <41826858+khatoonfatima@users.noreply.github.com>
2020-06-12 23:09:06 +0530

    Add files via upload

---
+++ Basic Knowledge of Python .py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#Limitations of Python:
+#1.Performance wise not upto the mark
+#2.Not using the mobile application
+
+
+# In[5]:
+
+
+#Flavors of Python:
+#1.CPython
+#2.Jython or JPython
+#3.IronPython
+#4.Pypy
+#5.RubyPython
+#6.AnacondaPython
+
+
+# In[6]:
+
+
+########################################     IDENTIFIERS        ################################################
+# A name in Python program is called IDENTIFIER
+# Alphabet Symbol (Either Upper or lower)
+#Identifier starts with the (_)then it indicates its PRIVATE
+# $ symbol is not allowed in Python
+
+
+# In[7]:
+
+
+######################################## RESERVED WORDS     #################################################
+
+#In Python some words are reservedto represent some meaning or functionality.Such type of words is called Reserved Words
+
+####  All reserved word in python contain alphabet symbols
+#### except the following three word 
+  #####TRUE
+    #####FALSE
+    ###### NONE
+    
+
+
+# In[8]:
+
+
+###### RESERVED WORD OR KEYWORDS    #########
+
+import keyword
+keyword.kwlist
+
+
+# In[3]:
+
+
+###### DATA TYPES #######
+#### Data type represent the type of data in the variable,in python we are not required to represent the data type explicitly
+9#Based on the value provided the type will be automatically assigned
+#Python has several inbuilt functions
+#type():to check the type of the variable
+#id():to get address of the object
+#print():to print the value of the object
+#######**********In Python everything is object
+
+#int data type:
+#We can use int data type to represent whole numbers(integral values)
+#Eg:
+a=10
+type(a)
+#We can represent the integral value in following forms like
+#1.Decimal
+#2.Binary
+#3.Octal
+#4.Hexa decimal
+
+
+# In[5]:
+
+
+#Decimal Form(base-10)
+#It is default system in Python
+#The allowed digits are:0 to 9
+#eg
+a=10
+print(a)
+
+
+# In[16]:
+
+
+#Binary Form(base-2):
+#The allowed digits are:0 & 1
+#Literal values should be prefixed with 0b or 0B
+a=0B1111
+a=0B111
+a=0B11111
+print(a)
+
+
+# In[ ]:
+
+
+#Octal Form(base-3  )
+
+
+# In[ ]:
+
+
+
+
