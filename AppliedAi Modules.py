5aad9aca485090f43f10394250be6145c601e5e9
Fatima Khatoon <41826858+khatoonfatima@users.noreply.github.com>
2020-06-14 06:17:18 +0530

    Add files via upload

---
+++ AppliedAi Modules.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # MODULES
+
+# In[2]:
+
+
+import math
+
+print(math.pi)
+
+
+# In[4]:
+
+
+import math as m
+print(m.pi)
+
+
+# In[10]:
+
+
+import datetime as dt
+print(dt.datetime.now())
+
+
+# In[13]:
+
+
+from datetime import datetime as dt
+print(dt.now())
+
+
+# In[14]:
+
+
+dir(math)
+
